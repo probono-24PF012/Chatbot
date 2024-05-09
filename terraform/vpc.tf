7527daf732fd9946038404d27a3ef6c212f6f385
kyounghoonJang <matkimchi_@naver.com>
2024-05-10 02:29:20 +0900

    Spring-GKE-Loadbalancer

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,22 +1,22 @@
-resource "google_compute_network" "project_vpc" {
-  name = "projcet-vpc"
-  auto_create_subnetworks = "false"
-  routing_mode = "REGIONAL"
-  description = "VPC for the project"
-  mtu = 1460
-}
+# resource "google_compute_network" "project_vpc" {
+#   name = "projcet-vpc"
+#   auto_create_subnetworks = "false"
+#   routing_mode = "REGIONAL"
+#   description = "VPC for the project"
+#   mtu = 1460
+# }
 
 
-resource "google_compute_subnetwork" "public_subnet" {
-  name = "public-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  region = "asia-northeast3"
-  network = google_compute_network.project_vpc.id
-}
-resource "google_compute_subnetwork" "private_subnet" {
-  name = "private-subnet"
-  ip_cidr_range = "10.0.2.0/24"
-  region = "asia-northeast3"
-  network = google_compute_network.project_vpc.id
-  private_ip_google_access= "true"
-}
+# resource "google_compute_subnetwork" "public_subnet" {
+#   name = "public-subnet"
+#   ip_cidr_range = "10.0.1.0/24"
+#   region = "asia-northeast3"
+#   network = google_compute_network.project_vpc.id
+# }
+# resource "google_compute_subnetwork" "private_subnet" {
+#   name = "private-subnet"
+#   ip_cidr_range = "10.0.2.0/24"
+#   region = "asia-northeast3"
+#   network = google_compute_network.project_vpc.id
+#   private_ip_google_access= "true"
+# }
